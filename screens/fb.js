c9cfaddc2ef7a1f71b0281d93756a974ff90ab3e
Menal Kour <74865342+MenalKour@users.noreply.github.com>
2021-04-15 21:30:47 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+
+
+export default class TransactionScreen extends React.Component{
+    
+    render(){
+        return(
+            <View>
+                <Text style={{
+               background:'lightblue',
+               marginTop:280,
+                fontSize:30,
+                alignSelf:'center',
+                justifyContent:'center',
+                
+            }}>
+                             
+                    ✨FACEBOOK✨   |  FB😊👍  |
+                              
+                 </Text>
+            </View>
+        );
+    }
+}
---
+++ screens/in.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View >
+                <Text
+                style={{
+                
+               marginTop:280,
+               background:'lightpink',
+                fontSize:30,
+                alignSelf:'center',
+                justifyContent:'center'
+            
+            }}>
+                    ❇INSTAGRAM❇  |  IG💟™ | 
+                </Text>
+            </View>
+        );
+    }
+}
+
